d3c71ba6f6e61c86d35ed420a0735fe44af7e272
Eymard Neto <francisco.eymard.oliveira@gmail.com>
2021-02-22 22:31:07 -0300

    fix: _app ts error

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+import ExperienceBar from "../src/components/ExperienceBar" 
+export default function Home(){
+  return (
+    <div className="container">
+      <ExperienceBar/>
+    </div>
+  )
+}
